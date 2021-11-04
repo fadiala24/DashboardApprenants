e6669afd14aa63b8ca6853f8629c4d532f8a02fe
Christophe03 <81380467+Christophe03@users.noreply.github.com>
2021-11-04 18:59:44 +0000

    Front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormateurComponent } from './formateur/formateur.component';
+import { ModifierComponent } from './modifier/modifier.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { SupprimerComponent } from './supprimer/supprimer.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes =[
+  {path:'modifier', component: ModifierComponent},
+  {path:'tableau', component: DashboardComponent},
+  {path:'formateur', component: FormateurComponent},
+  {path:'register', component: RegisterComponent},
+  {path: 'supprimer', component: SupprimerComponent},
+  {path:'', component: NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from './register/register.component';
+import { SupprimerComponent } from './supprimer/supprimer.component';
+import { ModifierComponent } from './modifier/modifier.component';
+import { FormateurComponent } from './formateur/formateur.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    SupprimerComponent,
+    ModifierComponent,
+    FormateurComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
